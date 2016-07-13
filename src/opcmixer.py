ee06ddb52e8f9734e538d49525b20722fe73462d
Shen Tian <biozinc@gmail.com>
2016-07-13 21:43:06 +0200

    OPC forwarding working. Does some basic color manipulation

--- src/opcmixer.py
+++ src/opcmixer.py
@@ -1,7 +1,14 @@
 import SocketServer
+import socket
+import time
+import math
 import struct
 
+
 class OpcRequestHandler(SocketServer.BaseRequestHandler):
+
+    def setup(self):
+        print "Setup"
 
     def handle(self):
         print "Connected from", self.client_address
@@ -9,32 +16,55 @@ class OpcRequestHandler(SocketServer.BaseRequestHandler):
         try:
             new_conn = True
 
+            out_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            out_socket.settimeout(1)
+            out_socket.connect(('127.0.0.1', 7891))
+
             while True:
                 header = self.request.recv(4)
                 if header == '':
                     break;
-                (channel, cmd, n) = struct.unpack('>BBH', header)
+                (channel, cmd, n) = struct.unpack('!BBH', header)
+
+                frame = bytearray(self.request.recv(n))
 
                 if cmd == 0:
                     if new_conn:
                         print n/3
                         new_conn = False
 
-                data = self.request.recv(n)
+                    millis = int(round(time.time() * 1000))
+
+                    scale = (math.sin(millis * .001) + 1) * .5
+                    #print scale
+                    new_frame = []
+
+                    for i in range(0, 1560):
+                        r = frame[i * 3]
+                        g = frame[i * 3 + 1]
+                        b = frame[i * 3 + 2]
+
+                        r = int(r * scale)
+                        g = int(g * scale)
+                        #b = int(b * scale)
+
+                        new_frame.append(chr(r) + chr(g) + chr(b))
+
+                    out_socket.send(header)
+                    out_socket.send(''.join(new_frame))
+
         finally:
             print "Fin"
             return
 
 
-
 class Server(object):
 
     def __init__(self, port):
+
         address = ('0.0.0.0', 7890)
-        server = SocketServer.TCPServer(address, OpcRequestHandler)
+        server = SocketServer.ThreadingTCPServer(address, OpcRequestHandler)
 
         server.serve_forever()
 
-
-print "Hello world"
 Server(7890)
