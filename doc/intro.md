2b4609312f79afcb597c17aec1088e6621b70340
Shen Tian <biozinc@gmail.com>
2016-10-13 22:43:12 +0200

    Starting over, in clojure

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to opcmixer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/opcmixer/core_test.clj
@@ -0,0 +1,7 @@
+(ns opcmixer.core-test
+  (:require [clojure.test :refer :all]
+            [opcmixer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
