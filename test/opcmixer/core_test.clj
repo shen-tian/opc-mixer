a0f1ded4a5ca546c723d32a045957efaa5120edd
Shen Tian <biozinc@gmail.com>
2016-10-14 00:00:42 +0200

    Fix the test (haha)

--- test/opcmixer/core_test.clj
+++ test/opcmixer/core_test.clj
@@ -1,7 +1,7 @@
 (ns opcmixer.core-test
   (:require [clojure.test :refer :all]
-            [opcmixer.core :refer :all]))
+            [opcmixer.mixer :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
