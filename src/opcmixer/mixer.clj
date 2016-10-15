13fe37f85c23877707f42872ab0971aafcdadd87
Shen Tian <biozinc@gmail.com>
2016-10-15 17:41:00 +0200

    Got multi-stream working

--- src/opcmixer/mixer.clj
+++ src/opcmixer/mixer.clj
@@ -6,7 +6,9 @@
    [aleph.tcp :as tcp]
    [aleph.http :as http]
    [gloss.core :as gloss]
-   [gloss.io :as io])
+   [gloss.io :as io]
+   [compojure.core :as compojure :refer [GET]]
+   [compojure.route :as route])
   (:gen-class))
 
 ;; The OPC protocol. Thanks gloss!
@@ -39,35 +41,47 @@
   (d/chain (tcp/client {:host host, :port port})
            #(wrap-duplex-stream opc-protocol %)))
 
-(def x (atom []))
 
-(def i-ch (atom {}))
+(defn add-stream [id]
+  (fn [m]
+    (assoc m (keyword id) '{:name "name"})))
 
-(defn print-handler
-  [opc-struct]
-  (let [len (count (nth opc-struct 2))]  
-    (if (= len 5)
-      (prn "special: " (str opc-struct))
-      (swap! i-ch (fn [i] {:timestamp (System/currentTimeMillis)
-                           :colors (take 10 (nth opc-struct 2))})))
-    ;;(prn (System/currentTimeMillis))
-    (swap! x #(conj % (System/currentTimeMillis)))
-    ;;(prn len)
-    ))  
+(defn update-frame [id frame]
+  (fn [m]
+    (let [key (keyword id)]
+      (update m key
+              (fn [stream] (assoc stream :frame frame))))))
 
-(defn my-handler
+(defn remove-stream [id]
+  (fn [m]
+    (dissoc m (keyword id))))
+
+(def opc-streams (atom '{})) ;;   
+
+(defn handler
   "OPC Handler."
   [s info]
-  (s/on-drained s #(prn "closed"))
-  (prn (str "new conn: "(:remote-addr info)))
-  (s/consume print-handler s))
-
+  (let [id (str (System/currentTimeMillis))]
+    (s/on-drained s (fn [] 
+                      (prn "closed")
+                      (swap! opc-streams (remove-stream id))))
+    (prn (str "new conn: "(:remote-addr info)))
+    (swap! opc-streams (add-stream id))
+    (s/consume (fn [opc-struct]
+                 (let [frame (nth opc-struct 2)]
+                   (if (= (count frame) 5)
+                     (prn "special: " (str frame))
+                     (swap! opc-streams (update-frame id frame))))) s)))
 
 (defn hello-world-handler
   [req]
   {:status 200
    :headers {"content-type" "text/plain"}
-   :body (str "hello world!" @i-ch)})
+   :body (str @opc-streams)})
+
+(compojure/defroutes app-routes
+  (GET "/" [] hello-world-handler)
+  (GET "/color/" [] (str @i-ch)))
 
 (defn start-web
   []
@@ -76,7 +90,7 @@
 (defn -main
   "Entry point."
   [& args]
-  (def s (start-server my-handler 7890))
+  (def s (start-server handler 7890))
   ;;(def s2 (http/start-server hello-world-handler {:port 10000}))
   ;;(def ping (s/periodically 500 (fn [] "hi")))
   ;;(s/consume #(prn %) ping)
